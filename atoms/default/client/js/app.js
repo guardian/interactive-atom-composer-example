510353295c470961e4d1154a4994db8566476207
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-08-20 11:35:10 +0200

    add example

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,2 +1,22 @@
 // if you want to import a module from shared/js then you can
 // just do e.g. import Scatter from "shared/js/scatter.js"
+
+const quotesAll = document.querySelectorAll('figure+h2+blockquote');
+quotesAll.forEach((quoteEl) => {
+  const mainEl = quoteEl.parentElement;
+  const h2El = quoteEl.previousElementSibling;
+  const figureEl = h2El.previousElementSibling;
+
+  console.log('NEW GROUP');
+  console.log(quoteEl, h2El, figureEl);
+
+  let wrapperEl = document.createElement('div');
+  wrapperEl.classList.add('sticky-wrapper');
+
+  mainEl.insertBefore(wrapperEl, figureEl);
+
+  wrapperEl.appendChild(figureEl);
+  wrapperEl.appendChild(h2El);
+  wrapperEl.appendChild(quoteEl);
+
+})
